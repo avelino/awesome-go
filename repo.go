25384cc0c7404e0e8b19467300694c8184d01bba
Kirill Danshin <k@guava.by>
2016-10-11 01:53:49 +0300

    fix linter errors, remove unnecessary conversion, add binary to .gitignore

--- repo.go
+++ repo.go
@@ -11,31 +11,31 @@ import (
 	"github.com/russross/blackfriday"
 )
 
-type Content struct {
+type content struct {
 	Body string
 }
 
-func GenerateHtml() {
+func generateHTML() {
 	// Update repo
 	exec.Command("git", "checkout", "-f").Output()
 	exec.Command("git", "pull").Output()
 
 	input, _ := ioutil.ReadFile("./README.md")
-	body := string(blackfriday.MarkdownCommon([]byte(string(input))))
-	c := &Content{Body: body}
+	body := string(blackfriday.MarkdownCommon(input))
+	c := &content{Body: body}
 
 	t := template.Must(template.ParseFiles("tmpl/tmpl.html"))
 	f, _ := os.Create("tmpl/index.html")
 	t.Execute(f, c)
 }
 
-func HookHandler(w http.ResponseWriter, r *http.Request) {
-	go GenerateHtml()
+func hookHandler(w http.ResponseWriter, r *http.Request) {
+	go generateHTML()
 	w.Write([]byte("Done!\n"))
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/hook", HookHandler)
+	r.HandleFunc("/hook", hookHandler)
 	http.ListenAndServe(":9000", r)
 }
