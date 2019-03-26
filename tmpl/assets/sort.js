b42a5e4309149d2922469c08596657b5c7df8f93
Ma_124 <ma_124@pm.me>
2019-03-26 18:40:55 +0100

    refactored /tmpl/assets/sort.js#comparer for codeclimate
    pleases https://codeclimate.com/github/avelino/awesome-go/tmpl/assets/sort.js/source?from_sha=d898b549&to_sha=bb9bb5e4

--- tmpl/assets/sort.js
+++ tmpl/assets/sort.js
@@ -1,8 +1,13 @@
 const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
-const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
-    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+const comparer = function (idx, asc) {
+	return (a, b) => ((v1, v2) => {
+		if (v1 == '' || v2 == '' || isNaN(v1) || isNaN(v2)) {
+			return v1.toString().localeCompare(v2);
+		}
+		return v1 - v2;
+	})(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+};
 
 document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
 	tbls = document.getElementsByTagName('table');
