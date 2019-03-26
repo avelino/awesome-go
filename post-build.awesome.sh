bb9bb5e4c3558dbe8b594080da84b093c2726676
Ma_124 <ma_124@pm.me>
2019-03-26 17:31:38 +0100

    now renders gh-pages using awesome framework

---
+++ post-build.awesome.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+mkdir -p public
+cp -r tmpl/** public
+
+cat <<-EOF > public/index.html
+<!DOCTYPE html>
+<html class="sorted-by-5-asc">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="Content-Language" content="en">
+        <meta name="viewport" content="width=device-width">
+    
+        <title>Awesome Go</title>
+    
+        <meta name="description" content="A curated list of awesome Go frameworks, libraries and software">
+        <meta name="keywords" content="golang, go, awesome, awesome-go, go framework, golang framework">
+        <meta name="twitter:card" value="summary">
+    
+        <meta property="og:title" content="Awesome Go" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="https://awesome-go.com/" />
+        <meta property="og:image" content="https://awesome-go.com/assets/logo.png" />
+        <meta property="og:description" content="A curated list of awesome #Golang frameworks, libraries and software" />
+    
+        <link rel="stylesheet" type="text/css" href="assets/fonts/firasans.css">
+        <link rel="stylesheet" type="text/css" href="assets/style.css">
+    </head>
+    <body>
+        <img src="/assets/logo.png" alt="Awesome Go" class="awesome-logo" />
+    
+        <div id="content">
+            $(cat public/index.html)
+        </div>
+    
+        <script src="assets/jquery-custom.min.js"></script>
+        <script src="assets/marked.js"></script>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+          ga('create', 'UA-85465107-1', 'auto');
+          ga('send', 'pageview');
+        </script>
+        <script src="assets/sort.js"></script>
+    </body>
+</html>
+EOF
---
+++ tmpl/assets/sort.js
@@ -0,0 +1,24 @@
+const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+	tbls = document.getElementsByTagName('table');
+    const thi = Array.from(th.parentNode.children).indexOf(th);
+
+    this.asc = !this.asc;
+
+    if (this.asc) {
+        document.getElementsByTagName('html')[0].className = 'sorted-by-' + (thi + 1) + '-asc';
+    } else {
+        document.getElementsByTagName('html')[0].className = 'sorted-by-' + (thi + 1) + '-desc';
+    }
+
+    for (let i = 0; i < tbls.length; i++) {
+	    const tbody = tbls[i].querySelector('tbody');
+	    Array.from(tbody.querySelectorAll('tr')).sort(comparer(thi, this.asc)).forEach(tr => tbody.appendChild(tr));
+    }
+})));
+
