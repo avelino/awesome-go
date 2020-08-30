86cb0d98ce72886627733efcdf71ebfaf4aafdb3
Tony Kappen <tkappen@edu.uwaterloo.ca>
2020-08-30 13:19:39 -0400

    fixes #3211 handle status code 302 and 301

--- test_stale_repositories.go
+++ test_stale_repositories.go
@@ -174,18 +174,16 @@ func testCommitAge(href string, oauthClient *http.Client, staleRepos *[]string, 
 		if resp.StatusCode == 301 {
 			*staleRepos = append(*staleRepos, href+movedPermanently)
 			log.Printf("%s returned 301", href)
-			return
 		} else if resp.StatusCode == 302 {
 			*staleRepos = append(*staleRepos, href+status302)
 			log.Printf("%s returned 302", href)
-			return
 		} else if resp.StatusCode >= 400 {
 			*staleRepos = append(*staleRepos, href+deadLinkMessage)
 			log.Printf("%s might not exist!", href)
-			return
+		} else {
+			json.NewDecoder(resp.Body).Decode(&respObj)
+			isAged = len(respObj) == 0
 		}
-		json.NewDecoder(resp.Body).Decode(&respObj)
-		isAged = len(respObj) == 0
 		if isAged {
 			log.Printf("%s has not had a commit in a while", href)
 			*staleRepos = append(*staleRepos, href)
